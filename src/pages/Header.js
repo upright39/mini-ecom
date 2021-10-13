394f28d38a58369da1b37fc763a28b5d77042853
uprightman <uprightnzubechi@gmail.com>
2021-10-13 18:56:18 +0100

    second commit

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Typed from "react-typed"
 export const Header = () => {
     return (
-        <div>
-            <h2>header one</h2>
+        <div className='header-wrapper'>
+            <div className='main-wrapper'>
+                <h1>you are welcome to our world</h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Welcome to our store", "Buy at an afrdable price", "Promotion on the way"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+            </div>
         </div>
     )
 }
